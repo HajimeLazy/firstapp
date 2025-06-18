166d547c16bb45ed7ddc2e003710e8f3e944387a
Tata <tatath214@gmail.com>
2025-06-18 12:01:08 +0700

    fix text

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-    <h1 className="text-4xl text-center text-blue-700"> Home Page </h1>
+    <h1 className="text-4xl text-center text-blue-700"> กิตติคุณ ชูบุญ </h1>
     </>
   );
 }
